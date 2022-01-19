db68fb5c2eefde90a892bbeee6d4e6075a1c4104
darshvader13 <dbalani2002@gmail.com>
2022-01-18 20:15:13 -0800

    uwu

--- public/js/main.js
+++ public/js/main.js
@@ -8,7 +8,7 @@ const { username, color } = Qs.parse(location.search, {
 });
 
 let room = 'room';
-const socket = io.connect();
+const socket = io();
 
 // Join chatroom
 socket.emit('joinRoom', { username, room, color });
